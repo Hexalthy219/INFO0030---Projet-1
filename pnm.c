6ef071f190298bd9becbd94b14aab7e7ae8e1904
Russe Cyril <cyril.russe@hotmail.com>
2020-02-18 23:38:21 +0100

    Initialisation pnm.c

---
+++ pnm.c
@@ -0,0 +1,43 @@
+/**
+ * pnm.c
+ * 
+ * Ce fichier contient les définitions de types et 
+ * les fonctions de manipulation d'images PNM.
+ * 
+ * @author: Russe Cyril s170220
+ * @date: 18-02-2020
+ * @projet: INFO0030 Projet 1
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "pnm.h"
+
+
+/**
+ * Définition du type opaque PNM
+ *
+ */
+struct PNM_t {
+
+   /* Insérez ici les champs de la structure PNM */
+
+};
+
+
+int load_pnm(PNM **image, char* filename) {
+
+   /* Insérez le code ici */
+
+   return 0;
+}
+
+int write_pnm(PNM *image, char* filename) {
+
+   /* Insérez le code ici */
+
+   return 0;
+}
+
